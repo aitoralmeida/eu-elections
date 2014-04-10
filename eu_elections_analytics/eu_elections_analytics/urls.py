b62263b74dad224e3e83a3a3805d7dce9f988352
Oscar <oscar.pdr@gmail.com>
2014-04-11 01:00:14 +0200

    Hashtags for each candidate template added

--- eu_elections_analytics/eu_elections_analytics/urls.py
+++ eu_elections_analytics/eu_elections_analytics/urls.py
@@ -9,5 +9,22 @@ urlpatterns = patterns('',
     url(r'^group_representation_by_country/$', 'eu_elections_analytics.views.group_representation_by_country', name='group_representation_by_country'),
     url(r'^geo_group_representation/$', 'eu_elections_analytics.views.geo_group_representation', name='geo_group_representation'),
 
+    # HASHTAGS
+        # Candidates
+    url(r'^hashtags/candidato/(?P<candidate_screen_name>\S+)/$', 'eu_elections_analytics.views.hashtags_by_candidate', name='hashtags_by_candidate'),
+    # url(r'^hashtags/candidato/$', 'eu_elections_analytics.views.hashtags_candidate_index', name='hashtags_candidate_index'),
+    #     # Country
+    # url(r'^hashtags/pais/(?P<country_slug>\S+)/$', 'eu_elections_analytics.views.hashtags_by_country', name='hashtags_by_country'),
+    # url(r'^hashtags/pais/$', 'eu_elections_analytics.views.hashtags_country_index', name='hashtags_country_index'),
+    #     # Group
+    # url(r'^hashtags/grupo_parlamentario/(?P<group_slug>\S+)/$', 'eu_elections_analytics.views.hashtags_by_group', name='hashtags_by_group'),
+    # url(r'^hashtags/grupo_parlamentario/$', 'eu_elections_analytics.views.hashtags_group_index', name='hashtags_group_index'),
+    #     # Evolution
+    # url(r'^hashtags/evolucion/$', 'eu_elections_analytics.views.hashtags_evolution', name='hashtags_evolution'),
+    #     # Index
+    # url(r'^hashtags/$', 'eu_elections_analytics.views.hashtags', name='hashtags'),
+
+    # url(r'^foo/$', 'eu_elections_analytics.views.foo', name='foo'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
--- eu_elections_analytics/eu_elections_analytics/views.py
+++ eu_elections_analytics/eu_elections_analytics/views.py
@@ -3,6 +3,8 @@
 
 from django.shortcuts import render
 
+from eu_elections_analytics.models import Users, HashCandidate, Groups
+
 
 # Create your views here.
 
@@ -12,7 +14,26 @@ from django.shortcuts import render
 ####################################################################################################
 
 def home(request):
-    return render(request, "eu_elections_analytics/index.html")
+    groups = Groups.objects.all()
+
+    candidate_ids_set = set()
+
+    for group in groups:
+        candidates_set.add(group.candidate_id)
+        try:
+            candidates_set.add(group.subcandidate_id)
+        except:
+            pass
+
+    candidate_ids = list(candidate_ids_set)
+
+    candidates = Users.objects.filter(id__in=candidate_ids)
+
+    return_dict = {
+        'candidates': candidates,
+    }
+
+    return render(request, "eu_elections_analytics/index.html", return_dict)
 
 
 ####################################################################################################
@@ -29,3 +50,50 @@ def group_representation_by_country(request):
 
 def geo_group_representation(request):
     return render(request, "eu_elections_analytics/geo_group_representation.html")
+
+
+
+####################################################################################################
+####################################################################################################
+#####   HASHTAGS
+####################################################################################################
+####################################################################################################
+
+
+####################################################################################################
+#####   View: hashtags_by_candidate()
+####################################################################################################
+
+def hashtags_by_candidate(request, candidate_screen_name):
+    candidate = Users.objects.get(screen_name=candidate_screen_name)
+    hashtags = HashCandidate.objects.filter(candidate_id=candidate.id)
+
+    return_dict = {
+        'candidate': candidate,
+        'hashtags': hashtags,
+    }
+    return render(request, "eu_elections_analytics/hashtags/by_candidate.html", return_dict)
+
+
+####################################################################################################
+#####   View: hashtags_candidate_index()
+####################################################################################################
+
+def hashtags_candidate_index(request, country_slug):
+    return render(request, "eu_elections_analytics/hashtags/by_country.html")
+
+
+####################################################################################################
+#####   View: hashtags_by_candidate()
+####################################################################################################
+
+def hashtags_by_candidate(request, candidate_slug):
+    return render(request, "eu_elections_analytics/hashtags/by_candidate.html")
+
+
+####################################################################################################
+#####   View: hashtags_by_candidate()
+####################################################################################################
+
+def hashtags_by_candidate(request, candidate_slug):
+    return render(request, "eu_elections_analytics/hashtags/by_candidate.html")
