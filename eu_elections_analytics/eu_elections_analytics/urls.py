5847fee15ac30d2d73ee561d3ee3a81af729f034
Oscar <oscar.pdr@gmail.com>
2014-04-10 19:05:57 +0200

    Removed unused visualizations (graphs over maps)

--- eu_elections_analytics/eu_elections_analytics/urls.py
+++ eu_elections_analytics/eu_elections_analytics/urls.py
@@ -7,11 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', 'eu_elections_analytics.views.home', name='home'),
 
     url(r'^group_representation_by_country/$', 'eu_elections_analytics.views.group_representation_by_country', name='group_representation_by_country'),
-    url(r'^party_representation_on_map/$', 'eu_elections_analytics.views.party_representation_on_map', name='party_representation_on_map'),
-
-    url(r'^map1/$', 'eu_elections_analytics.views.map1', name='map1'),
-    url(r'^map2/$', 'eu_elections_analytics.views.map2', name='map2'),
-    url(r'^map3/$', 'eu_elections_analytics.views.map3', name='map3'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- eu_elections_analytics/eu_elections_analytics/views.py
+++ eu_elections_analytics/eu_elections_analytics/views.py
@@ -21,35 +21,3 @@ def home(request):
 
 def group_representation_by_country(request):
     return render(request, "eu_elections_analytics/group_representation_by_country.html")
-
-
-####################################################################################################
-#####   View: party_representation_on_map()
-####################################################################################################
-
-def party_representation_on_map(request):
-    return render(request, "eu_elections_analytics/party_representation_on_map.html")
-
-
-####################################################################################################
-#####   View: map1()
-####################################################################################################
-
-def map1(request):
-    return render(request, "eu_elections_analytics/map1.html")
-
-
-####################################################################################################
-#####   View: map2()
-####################################################################################################
-
-def map2(request):
-    return render(request, "eu_elections_analytics/map2.html")
-
-
-####################################################################################################
-#####   View: map3()
-####################################################################################################
-
-def map3(request):
-    return render(request, "eu_elections_analytics/map3.html")
