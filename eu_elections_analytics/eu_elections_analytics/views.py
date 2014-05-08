dbc6c3d0c116d1387d54db5191f01ab2f97d6d02
Oscar <oscar.pdr@gmail.com>
2014-05-08 14:35:12 +0200

    Dealing with the incorrect name displaying bug

--- eu_elections_analytics/eu_elections_analytics/views.py
+++ eu_elections_analytics/eu_elections_analytics/views.py
@@ -74,15 +74,11 @@ def home(request):
 
             for party in group_data['parties']:
                 if party['id'] == 0:
-                    party_screen_name = 'No_TWITTER_ACCOUNT'
+                    party['name'] = '-'
                 else:
-                    query = "SELECT screen_name FROM twitter_users WHERE id = '%s'" % party['id']
-                    cursor.execute(query)
+                    cursor.execute("SELECT screen_name FROM twitter_users WHERE id = '%s'" % party['id'])
                     for result in cursor:
-                        print result
-                        party_screen_name = result[0]
-
-                    party['name'] = party_screen_name
+                        party['name'] = result[0]
 
             cursor.execute("Select eu_total, co_total from europe_group where group_id = '%s'" % group)
             for result in cursor:
