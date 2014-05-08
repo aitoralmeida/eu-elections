322ab666413eb3af44ad9a5aeeea3789988cdf15
Oscar <oscar.pdr@gmail.com>
2014-05-08 14:53:58 +0200

    Included settings configuration to have debug_toolbar working

--- eu_elections_analytics/eu_elections_analytics/settings/base.py
+++ eu_elections_analytics/eu_elections_analytics/settings/base.py
@@ -94,4 +94,19 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
+DEBUG_TOOLBAR_PATCH_SETTINGS = DEBUG
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
